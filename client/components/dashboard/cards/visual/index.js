e05c35ff3102800c768549a2139f075e9a5898d1
Alberto <mail4alberto@gmail.com>
2015-09-17 02:32:06 -0400

    cosmetic prototyping

--- client/components/dashboard/cards/visual/index.js
+++ client/components/dashboard/cards/visual/index.js
@@ -25,15 +25,52 @@ const CardVisual = React.createClass({
                 <div className="row">
                     <div className="col-sm-12">
                         <div className="card">
-                            <div className="card-block p-b-0">
-                                <h4 className="card-title">{this.props.title}</h4>
+                            <div className="card-block">
+                                <h4 className="card-title m-y-0">{this.props.title}</h4>
 
                                 <Preview text={this.props.description} />
-                            </div>
 
-                            <div className="card-header p-y-0">
-                                {""}
+                                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
                             </div>
+                        </div>
+
+                        <div className="card">
+                            <div className="card-header">
+                                <strong>{"Stats"}</strong>
+                            </div>
+                            <div className="card-block">
+                                <div className="container">
+                                    <div className="row">
+                                        <div className="col-sm-2">
+                                            <p className="card-text">
+                                                <strong>Success: 123</strong>
+                                            </p>
+                                            <p className="card-text">
+                                                <strong>Fails: 123</strong>
+                                            </p>
+                                            <p className="card-text">
+                                                <strong>Total: 123</strong>
+                                            </p>
+                                            <hr/>
+                                            <p className="card-text">
+                                                <strong>Score: 123</strong>
+                                            </p>
+                                        </div>
+                                        <div className="col-sm-6">
+                                          {"Graph here"}
+                                        </div>
+                                        <div className="col-sm-4">
+                                          {"Histogram here"}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card-footer text-muted">
+                                <center>{"Reviewed 2 days ago"}</center>
+                            </div>
+                        </div>
+
+                        <div className="card">
                             <div className="card-header">
                                 <strong>{"Sides"}</strong>
                             </div>
